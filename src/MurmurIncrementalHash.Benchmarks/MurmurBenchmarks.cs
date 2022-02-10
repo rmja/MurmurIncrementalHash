a7ce07cca6785f99150a2b7ffbe8e65f00fdf8cd
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2022-02-10 13:51:40 +0100

    Add md5 comparison

--- src/MurmurIncrementalHash.Benchmarks/MurmurBenchmarks.cs
+++ src/MurmurIncrementalHash.Benchmarks/MurmurBenchmarks.cs
@@ -1,4 +1,5 @@
 ﻿using BenchmarkDotNet.Attributes;
+using System.Security.Cryptography;
 
 namespace MurmurIncrementalHash.Benchmarks;
 
@@ -8,6 +9,7 @@ public class MurmurBenchmarks
     private readonly Murmur32 _murmur32 = MurmurIncrementalHash.Murmur32.Create(0);
     private readonly Murmur _murmur128x64 = Murmur128.Create(0, Murmur128Algorithm.X64);
     private readonly Murmur _murmur128x86 = Murmur128.Create(0, Murmur128Algorithm.X86);
+    private readonly IncrementalHash _md5 = IncrementalHash.CreateHash(HashAlgorithmName.MD5);
     private readonly byte[] _data = new byte[] { 1, 2, 3, 4, 5, 6 };
 
     [Benchmark]
@@ -43,5 +45,17 @@ public class MurmurBenchmarks
 
         Span<byte> hash = stackalloc byte[16];
         _murmur128x86.GetHashAndReset(hash);
+    }
+
+    [Benchmark]
+    public void Md5()
+    {
+        _md5.AppendData(_data);
+        _md5.AppendData(_data);
+        _md5.AppendData(_data);
+        _md5.AppendData(_data);
+
+        Span<byte> hash = stackalloc byte[16];
+        _md5.GetHashAndReset(hash);
     }
 }
